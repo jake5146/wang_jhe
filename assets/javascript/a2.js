e9c89660dc3eb27d48d84957462146ff831b461e
JING WANG <jimwong234@gmail.com>
2016-06-04 00:13:04 -0400

    spaceship object

--- assets/javascript/a2.js
+++ assets/javascript/a2.js
@@ -47,7 +47,75 @@ function drawFramework(){
 	ctx.fillText("PAUSE",672,32);
 }
 
+//spaceship 50px*50px
+var spaceship = {
+	x:0,
+	y:0,
+	setCoordinate:function(x,y){
+		this.x = x;
+		this.y = y;
+	},
+	draw:function(){
+		//spaceship body
+	    ctx.save();
+	    ctx.translate(this.x,this.y);
+	    ctx.beginPath();
+	    ctx.moveTo(-10,-15);
+	    ctx.lineTo(10,-15);
+	    ctx.lineTo(10,15);
+	    ctx.lineTo(-10,15);
+	    ctx.closePath();
+	    ctx.stroke();
+	    //spaceship center
+	    ctx.fillStyle = "#4A9AB0";
+	    ctx.beginPath();
+	    ctx.moveTo(0,-10);
+	    ctx.lineTo(-5,0);
+	    ctx.lineTo(0,10);
+	    ctx.lineTo(5,0);
+	    ctx.closePath();
+	    ctx.fill();
+	    //spaceship head
+	    ctx.translate(0,-15);
+	    ctx.fillStyle = "#706E6E";
+	    ctx.beginPath();
+	    ctx.arc(0,0,10,Math.PI,0);
+	    ctx.fill();
+	    //spaceship wings
+	    var grd = ctx.createLinearGradient(0,0,0,60);
+	    grd.addColorStop(0,"#433DAF");
+	    grd.addColorStop(1,"white");
+	    ctx.fillStyle = grd;
+	    ctx.beginPath();
+	    ctx.moveTo(-10,0);
+	    ctx.lineTo(-25,30);
+	    ctx.lineTo(-10,30);
+	    ctx.closePath();
+	    ctx.fill();
+	    ctx.beginPath();
+	    ctx.moveTo(10,0);
+	    ctx.lineTo(25,30);
+	    ctx.lineTo(10,30);
+	    ctx.closePath(); 
+	    ctx.fill();
+	    //spaceship fire
+	    ctx.fillStyle = "#D14D38";
+	    ctx.translate(0,30);
+	    ctx.beginPath();
+	    ctx.moveTo(-25,0);
+	    ctx.lineTo(-17.5,10);
+	    ctx.lineTo(-10,0);
+	    ctx.fill();
+	    ctx.beginPath();
+	    ctx.moveTo(25,0);
+	    ctx.lineTo(17.5,10);
+	    ctx.lineTo(10,0);
+	    ctx.fill();
+	    ctx.restore();
+	}
+};
 
-
+spaceship.setCoordinate(100,100);
+spaceship.draw();
 
 
