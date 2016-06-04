021b6923c316b19db0608a28893c55f9fa98c646
JING WANG <jimwong234@gmail.com>
2016-06-03 21:32:41 -0400

    draw canvas framework

--- assets/javascript/a2.js
+++ assets/javascript/a2.js
@@ -1,3 +1,4 @@
+
 $(document).ready(function(){
 	//hide the canvas at the beginning
 	$("canvas").hide();
@@ -9,3 +10,44 @@ $(document).ready(function(){
 			$("canvas").show();
 		});
 });
+
+//draw canvas framework
+var canvas = document.getElementById("game");
+var ctx = canvas.getContext("2d");
+var score = 200;
+var seconds = 60;
+drawFramework();
+
+function drawFramework(){
+	//draw menu line
+	ctx.strokeStyle = "black";
+	ctx.beginPath();
+	ctx.moveTo(0,50);
+	ctx.lineTo(1000,50);
+	ctx.stroke();
+	//draw text
+	ctx.font = "35px sans-serif";
+	ctx.fillText(" Level #", 5, 35);
+	ctx.fillText("Score:",400,35);
+	ctx.fillText("seconds",850,35)
+	//draw number
+	ctx.fillText(score,510,35);
+	ctx.fillText(seconds,800,35)
+
+	//Pause button
+	ctx.beginPath();
+	ctx.moveTo(650,10);
+	ctx.lineTo(750,10);
+	ctx.lineTo(750,40);
+	ctx.lineTo(650,40);
+	ctx.closePath();
+	ctx.stroke();
+	//Pause text
+	ctx.font = "17px sans-serif";
+	ctx.fillText("PAUSE",672,32);
+}
+
+
+
+
+
